70b4fc59a360c6396e6772e49e7de28dda763d10
Lenovo <garvit.gupta_cs19@gla.ac.in>
2021-08-13 16:14:53 +0530

    VALTRI

---
+++ EasyProblemsToGetStarted/RajuAndHisTrip.java
@@ -0,0 +1,18 @@
+package EasyProblemsToGetStarted;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class RajuAndHisTrip {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner s = new Scanner(System.in);
+	    int N = s.nextInt();
+	    if (N%5==0 || N%6==0){
+	        System.out.println("YES");
+	    }
+	    else{
+	        System.out.println("NO");
+	    }
+	    // your code goes here
+	}
+}
